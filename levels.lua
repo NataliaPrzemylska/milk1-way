9bd787945fd6027590f4bfed13bd767696541e61
Wojtek <wojtek@localhost.localdomain>
2024-08-30 19:17:45 +0200

    level variables change when player exits map

--- levels.lua
+++ levels.lua
@@ -1,6 +1,28 @@
 mapX = 0
 mapY = 0
 
-function on_level_change()
+spawn_points = {
     
+}
+
+function on_level_change(right)
+    if right then
+        if mapX < 3 then
+            mapX += 1
+        else
+            mapY += 1
+            mapX = 0
+        end
+
+        player.x -= 128
+    else
+        if mapX > 0 then
+            mapX -= 1
+        else
+            mapY -= 1
+            mapX = 3
+        end
+        
+        player.x += 128
+    end
 end
