570f22dc931730d49604ac83bcbf02b5d72da2d0
wojtek <wojtek@dutch.com>
2024-08-31 19:54:51 +0200

    states

--- main.lua
+++ main.lua
@@ -8,51 +8,61 @@ function _init()
     temp_mapx = mapX
     temp_mapy = mapY
     exploded = 0
+    state="menu"
 end
 
 function _update()
-    for obj in all(current_objects) do
-        if day == obj.day then
-            obj.update(obj);
+    if state == "menu" then
+        if btn(4) then
+            state = "game"
         end
-    end
-    player_input()
-    update_level()
+    elseif state == "game" then
+        for obj in all(current_objects) do
+            if day == obj.day then
+                obj.update(obj);
+            end
+        end
+        player_input()
+        update_level()
 
-    if player.y > 130 then
-        dead()
+        if player.y > 130 then
+            dead()
+        end
     end
 end
 function _draw()
-    adjust_platforms()
-    adjust_platforms()
-    if day then
-        cls(15)
-    else
-        cls(0)
-    end
+    if state == "menu" then
 
-    draw_level()
-    draw_background()
-    -- draw a screen of tiles in map
-    map(mapX*16, mapY*16, 0, 0, 16, 16)
-
-    draw_player()
-
-    for obj in all(current_objects) do
-        if obj.day == day then
-            obj.draw(obj)
+    elseif state == "game" then
+        adjust_platforms()
+        if day then
+            cls(15)
+        else
+            cls(0)
         end
-    end
 
-    if player.has_key then
-        spr(133, 110, 5)
-    end
-    
+        draw_level()
+        draw_background()
+        -- draw a screen of tiles in map
+        map(mapX*16, mapY*16, 0, 0, 16, 16)
 
-    --debug
-    print(player.x,10,0)
-    print(player.y)
-    print(#current_objects)
-    print(exploded)
+        draw_player()
+
+        for obj in all(current_objects) do
+            if obj.day == day then
+                obj.draw(obj)
+            end
+        end
+
+        if player.has_key then
+            spr(133, 110, 5)
+        end
+        
+
+        --debug
+        print(player.x,10,0)
+        print(player.y)
+        print(#current_objects)
+        print(exploded)
+    end
 end
